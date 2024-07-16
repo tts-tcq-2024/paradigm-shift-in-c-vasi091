d7bbee7f677c3bd6a32aecdfc1ea138768268321
vasi091 <vasishtamukthinutalapati@gmail.com>
2024-07-16 10:42:42 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -1,21 +1,70 @@
 #include <stdio.h>
 #include <assert.h>
 
+typedef int (*CheckFunc)(float);
+
+typedef struct {
+    CheckFunc check;
+    float value;
+    const char *message;
+} Check;
+
+void printMessage(const char *message) {
+    printf("%s", message);
+}
+
+int isTemperatureInRange(float temperature) {
+    return (temperature >= 0 && temperature <= 45);
+}
+
+int isSocInRange(float soc) {
+    return (soc >= 20 && soc <= 80);
+}
+
+int isChargeRateInRange(float chargeRate) {
+    return (chargeRate <= 0.8);
+}
+
+int checkTemperature(float temperature) {
+    if (!isTemperatureInRange(temperature)) {
+        printMessage("Temperature out of range!\n");
+        return 0;
+    }
+    return 1;
+}
+
+int checkSoc(float soc) {
+    if (!isSocInRange(soc)) {
+        printMessage("State of Charge out of range!\n");
+        return 0;
+    }
+    return 1;
+}
+
+int checkChargeRate(float chargeRate) {
+    if (!isChargeRateInRange(chargeRate)) {
+        printMessage("Charge Rate out of range!\n");
+        return 0;
+    }
+    return 1;
+}
+
 int batteryIsOk(float temperature, float soc, float chargeRate) {
-  if(temperature < 0 || temperature > 45) {
-    printf("Temperature out of range!\n");
-    return 0;
-  } else if(soc < 20 || soc > 80) {
-    printf("State of Charge out of range!\n");
-    return 0;
-  } else if(chargeRate > 0.8) {
-    printf("Charge Rate out of range!\n");
-    return 0;
-  }
-  return 1;
+    int isTempOk = checkTemperature(temperature);
+    int isSocOk = checkSoc(soc);
+    int isChargeRateOk = checkChargeRate(chargeRate);
+    return isTempOk && isSocOk && isChargeRateOk;
+}
+
+void runTests() {
+    assert(batteryIsOk(25, 70, 0.7));
+    assert(!batteryIsOk(50, 85, 0));
+    assert(!batteryIsOk(30, 85, 0));
+    assert(!batteryIsOk(25, 70, 0.9));
+    printf("All tests passed!\n");
 }
 
 int main() {
-  assert(batteryIsOk(25, 70, 0.7));
-  assert(!batteryIsOk(50, 85, 0));
+    runTests();
+    return 0;
 }
