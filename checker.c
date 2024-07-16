e1a0f18e188620e05db7aa8108a75cbde98ff045
vasi091 <vasishtamukthinutalapati@gmail.com>
2024-07-16 10:22:30 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -1,46 +1,54 @@
 #include <stdio.h>
 #include <assert.h>
+
 typedef int (*CheckFunc)(float);
- 
+
 typedef struct {
     CheckFunc check;
     float value;
     const char *message;
 } Check;
- 
+
 void printMessage(const char *message) {
     printf("%s", message);
 }
- 
+
 int isTemperatureInRange(float temperature) {
     return (temperature >= 0 && temperature <= 45);
 }
- 
+
 int isSocInRange(float soc) {
     return (soc >= 20 && soc <= 80);
 }
- 
+
 int isChargeRateInRange(float chargeRate) {
     return (chargeRate <= 0.8);
 }
- 
+
+int performCheck(Check check) {
+    if (!check.check(check.value)) {
+        printMessage(check.message);
+        return 0;
+    }
+    return 1;
+}
+
 int batteryIsOk(float temperature, float soc, float chargeRate) {
     Check checks[] = {
         {isTemperatureInRange, temperature, "Temperature out of range!\n"},
         {isSocInRange, soc, "State of Charge out of range!\n"},
         {isChargeRateInRange, chargeRate, "Charge Rate out of range!\n"}
     };
- 
+
     for (int i = 0; i < sizeof(checks) / sizeof(checks[0]); ++i) {
-        if (!checks[i].check(checks[i].value)) {
-            printMessage(checks[i].message);
+        if (!performCheck(checks[i])) {
             return 0;
         }
     }
- 
+
     return 1;
 }
- 
+
 int main() {
     assert(batteryIsOk(25, 70, 0.7));
     assert(!batteryIsOk(50, 85, 0));
